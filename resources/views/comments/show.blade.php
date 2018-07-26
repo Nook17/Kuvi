77fe0d7940013204d6ff7a75378c7eb7e364eea5
Nook17 <arek@nook17.pl>
2018-07-26 19:57:39 +0200

    Add Role

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
  <div class="row">
-  <div class="col-md-6 offset-md-3">
+  <div class="col-md-6 offset-md-3 {{ $post->trashed() ? ' trashed' : '' }}">
 
    @include('comments.single')
 
